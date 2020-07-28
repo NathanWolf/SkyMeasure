76f84ff3fc0547dcaf50c1d3f1322cd89284de4d
Nathan Wolf <nathan@elmakers.com>
2020-07-28 11:06:21 -0700

    Add a CLI script to auto-scale and crop screenshots (slideshow time!)

---
+++ cv/crop.php
@@ -0,0 +1,81 @@
+<?php
+
+if (PHP_SAPI !== 'cli') {
+    die('Must be run from the terminal.');
+}
+
+if (count($argv) < 3) {
+    die("Usage: crop.php <source folder> <target folder>\n");
+}
+
+$sourceFolder = $argv[1];
+$targetFolder = $argv[2];
+
+function execProcess($cmd, &$stdout=null, &$stderr=null) {
+    $proc = proc_open($cmd,[
+        1 => ['pipe','w'],
+        2 => ['pipe','w'],
+    ],$pipes);
+    $stdout = stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+    $stderr = stream_get_contents($pipes[2]);
+    fclose($pipes[2]);
+    return proc_close($proc);
+}
+
+$targetWidth = 640;
+$targetHeight = 1024;
+
+$iterator = new DirectoryIterator($sourceFolder);
+foreach ($iterator as $fileInfo) {
+    if ($fileInfo->isDot()) continue;
+    $filename = $fileInfo->getFilename();
+    $pathname = $fileInfo->getPath();
+    $sourceFile = $pathname . '/' . $filename;
+
+    echo "Reading $filename\n";
+    $responseCode = execProcess("python align.py --image $sourceFile", $results, $errors);
+    if ($responseCode !== 0) {
+        echo "   ERROR: $responseCode ($errors)\n";
+        continue;
+    }
+
+    $response = json_decode($results, true);
+    $image = imagecreatefromstring(file_get_contents($sourceFile));
+    if (!$image) {
+        echo "  Could not load file\n";
+        continue;
+    }
+
+    $cropped = imagecreatetruecolor($targetWidth, $targetHeight);
+
+    $width = imagesx($image);
+    $height = imagesy($image);
+    $aspectRatio = $width / $height;
+    $targetAspectRatio = $targetWidth / $targetHeight;
+
+    $left = $response['hair']['left'] - 32;
+    $right = $response['lantern']['right'] + 32;
+
+    $top = $response['lantern']['top'] - 32;
+    $bottom = $response['lantern']['bottom'] + 64;
+
+    $lanternSize = $response['lantern']['bottom'] - $response['lantern']['top'];
+    $hairRatio = ($response['hair']['top'] - $response['lantern']['top']) / $lanternSize;
+    $hairRatio = floor($hairRatio * 1000);
+    $targetFile = $targetFolder . $hairRatio . '_' . $filename;
+
+    $sourceWidth = $right - $left;
+
+    // Resize and crop
+    imagecopyresampled($cropped,
+                     $image,
+                     0,
+                     0,
+                     $left, $top,
+                     $targetWidth, $targetHeight,
+                     $sourceWidth, $sourceWidth / $targetAspectRatio);
+
+    imagejpeg($cropped, $targetFile, 80);
+    echo "  Wrote $targetFile\n";
+}
