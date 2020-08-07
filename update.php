5a454b07fd66db6128c20ab17a28b8112ac889ec
Nathan Wolf <nathan@elmakers.com>
2020-08-07 09:26:26 -0700

    Allow an hour for update.php to run

--- update.php
+++ update.php
@@ -1,5 +1,6 @@
 <?php
 header("Content-Type: text/plain; charset=utf-8");
+set_time_limit(60 * 60);
 $client = $_SERVER['REMOTE_ADDR'];
 if (strpos($client, '192.168.1') !== 0) {
     die(json_encode(array('success' => false, 'message' => 'Sorry, internet randos are not allowed to delete files!')));
